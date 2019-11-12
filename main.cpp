3e88819fe1d6676cbed4c6fd2e8b314d4c140f67
sy <sy@sy>
2019-11-12 21:47:46 +0300

    Producer consumer problem is cleared out.

--- main.cpp
+++ main.cpp
@@ -3,12 +3,13 @@
 #include <pthread.h>
 #include <random>
 #include <unistd.h>
-
+#include <vector>
+#include <algorithm> // for copy
+#include <iterator> // for ostream_iterator
 using namespace std;
 
-#define BUFFER_SIZE 100
 
-int buffer[BUFFER_SIZE];
+vector<int> buffer, consume_buffer;
 int index = 0;
 
 sem_t full, empty;
@@ -16,11 +17,11 @@ pthread_mutex_t mutex;
 
 void* produce(void* arg){
     while(1){
-        usleep(1);
+        sleep(1);
         sem_wait(&empty);
         pthread_mutex_lock(&mutex);
         int item = rand()%100;
-        buffer[index++] = item;
+        buffer.push_back(item);
         cout << " *** Produced " << item << endl;
         pthread_mutex_unlock(&mutex);
         sem_post(&full);
@@ -29,19 +30,25 @@ void* produce(void* arg){
 
 void* consume(void* arg){
     while(1){
-        usleep(1);
+        sleep(1);
         sem_wait(&full);
         pthread_mutex_lock(&mutex);
-        int item = buffer[--index];
+        int item = buffer.front();
+        buffer.erase (buffer.begin());
         cout << "Consumed " << item << endl;
         pthread_mutex_unlock(&mutex);
         sem_post(&empty);
+        consume_buffer.push_back(item);
+        if(consume_buffer.size()==10){
+        	consume_buffer.empty();
+        }
+        std::cout << "Size : " << consume_buffer.size()<< endl;
     }
 }
 
 int main(){
     pthread_t producer, consumer;
-    sem_init(&empty, 0, BUFFER_SIZE);
+    sem_init(&empty, 0, 65535);
     sem_init(&full, 0, 0);
     pthread_mutex_init(&mutex, NULL);
     pthread_create(&producer, NULL, produce, NULL);
